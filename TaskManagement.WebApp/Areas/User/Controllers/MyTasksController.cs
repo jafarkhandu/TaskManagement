95ae2ee926a3a852e2daf8e4ea72a0da200c6243
HatimSethiya <hatimsethiya54@gmail.com>
2026-09-23 12:41:58 +0530

    my task page

---
+++ TaskManagement.WebApp/Areas/User/Controllers/MyTasksController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskManagement.Infrastructure.Data;
+using TaskManagement.Infrastructure.Identity;
+
+namespace TaskManagement.WebApp.Areas.User.Controllers
+{
+    [Area("User")]
+    [Authorize(Roles = "User")]
+    public class MyTasksController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public MyTasksController(
+            ApplicationDbContext context,
+            UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user == null)
+                return Challenge();
+
+            var tasks = await _context.TaskItems
+                .AsNoTracking()
+                .Where(t => t.AssignedToUserId == user.Id)
+                .OrderBy(t => t.Status == "Completed")
+                .ThenBy(t => t.ExpectedEndDate)
+                .ThenByDescending(t => t.Id)
+                .ToListAsync();
+
+            return View(tasks);
+        }
+    }
+}
